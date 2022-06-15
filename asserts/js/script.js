b26219ba3091bc5d6abf070842fa886fbbb9235d
Ligia A. Constantino <cellerligia23@gmail.com>
2022-06-14 21:49:20 -0300

    Feita a Função de Adicionar Item, Limpar todos os Itens da Lista e excluir os itens específicos da lista.

--- asserts/js/script.js
+++ asserts/js/script.js
@@ -1,30 +1,33 @@
 const input = document.getElementById("inputText");
-const button = document.getElementById("btn");
 const list = document.getElementById("todoList");
 const itemList = document.querySelectorAll("label");
+const inputChecked = document.querySelectorAll(".inputChecked");
+const btnAddList = document.querySelector("#btnAdd");
+const btnRemoveAll = document.querySelector("#btnRemoveAll");
+const btnRemove = document.querySelectorAll(".btnRemove");
+const btnEdit = document.querySelectorAll(".btnEdit");
 
-function addItemList(){
+/* FUNÇÃO PARA ADICIONAR OS ITENS NA LISTA */
+btnAddList.addEventListener('click', () => {
   const label = document.createElement("label");
   label.classList.add("labelList");
   label.innerHTML = `
     <div class="divItemList">
-      <input type="checkbox"> ${input.value}</input>
+      <input type="checkbox" class="inputChecked"> ${input.value}</input>
     </div>
 
     <div id="divBtnList">
       <button 
         type="button" 
-        id="btnRemove"
-        class="btnList"
-        onclick="rmvItemList()"
+        class="btnList btnRemove"
+        onclick="removeItem(event)"
       >
         Exluir
       </button>
+
       <button 
         type="button" 
-        id="btnEdit"
-        class="btnList"
-        onclick="editItemList()"
+        class="btnList btnEdit"
       >
         Editar
       </button>
@@ -32,8 +35,22 @@ function addItemList(){
   `
   list.appendChild(label);
   input.value = "";
-}
+})
 
-function rmvItemList(){
+/* FUNÇÃO PARA REMOVER TODOS OS ITENS DA LISTA */
+btnRemoveAll.addEventListener('click', () => {
+  //console.log(list.parentNode);
+  list.parentNode.removeChild(list);
+  document.location.reload(true);
+}); 
 
+/* FUNÇÃO PARA REMOVER INDIVIDUALMENTE OS ITENS DA LISTA */
+const removeItem = (event) => {
+  const element = event.target;
+  //console.log(element.parentNode.parentNode);
+  element.parentNode.parentNode.remove();
 }
+/* FUNÇÃO PARA EDITAR INDIVIDUALMENTE OS ITENS DA LISTA */
+
+/* FUNÇÃO PARA LOCALSTOREGE */
+
